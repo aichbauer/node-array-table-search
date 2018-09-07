1b8c15904152325c8ba03dc6b41509b755330f83
aichbauer <l.aichbauer@gmail.com>
2018-09-07 14:19:43 +0200

    Style: eslint

--- src/modules/multi-column-search-array-table.js
+++ src/modules/multi-column-search-array-table.js
@@ -2,9 +2,9 @@ const checkIfValid = (data) => {
   if (!Array.isArray(data)) {
     throw new Error(`data has to be typeof: ${typeof []} data instanceof Array: ${[] instanceof Array} but got typeof: ${typeof data} data instanceof Array: ${data instanceof Array}`);
   } else if (
-    Array.isArray(data) &&
-    data.length > 0 &&
-    typeof data[0] !== 'object'
+    Array.isArray(data)
+    && data.length > 0
+    && typeof data[0] !== 'object'
   ) {
     throw new Error(`data has to be an array of objects but data[0] got typeof: ${typeof data[0]}`);
   }
@@ -23,15 +23,15 @@ const createTableCheck = (data, options, tableCheck) => {
       const currentSearch = searchEntry[1].type;
 
       data.forEach((item, i) => {
-        const currentItemValue = item[searchEntry[0]] ?
-          item[searchEntry[0]].toString().toLowerCase() :
-          '';
+        const currentItemValue = item[searchEntry[0]]
+          ? item[searchEntry[0]].toString().toLowerCase()
+          : '';
         if (currentItem === searchEntry[0]) {
           switch (currentSearch) {
             case 'exact':
               if (
-                currentItemValue === currentValue &&
-                currentValue.length > 0
+                currentItemValue === currentValue
+                && currentValue.length > 0
               ) {
                 table[i][idx] = true;
               } else if (currentValue.length === 0) {
@@ -43,8 +43,8 @@ const createTableCheck = (data, options, tableCheck) => {
             case 'includes':
             default:
               if (
-                currentItemValue.includes(currentValue) &&
-                currentValue.length > 0
+                currentItemValue.includes(currentValue)
+                && currentValue.length > 0
               ) {
                 table[i][idx] = true;
               } else if (currentValue.length === 0) {
@@ -68,10 +68,10 @@ export const multiColumnSearchArrayTable = (data, options) => {
   const opts = Object
     .entries(options)
     .filter((entry) => (
-      typeof entry[1] === 'object' &&
-      (
-        entry[1].value ||
-        entry[1].value === ''
+      typeof entry[1] === 'object'
+      && (
+        entry[1].value
+        || entry[1].value === ''
       )
     ));
 
